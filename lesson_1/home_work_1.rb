22b537f0d3c44007f8c5316e04dd717106a8707d
oksana.skalko <skalkomargo@gmail.com>
2019-11-09 17:51:13 +0300

    task 1 and 2

--- lesson_1/home_work_1.rb
+++ lesson_1/home_work_1.rb
@@ -1,3 +1,28 @@
-def test
-  
+require 'tzinfo'
+
+class HomeWork
+
+  COLORS = { red: 31, green: 32, yellow: 33, blue: 34, pink: 35 }.freeze
+
+  def self.timezone
+    tzinfo = TZInfo::Timezone.get(ARGV[0])
+    period = tzinfo.current_period
+    utc_offset = period.offset.utc_total_offset
+    puts Time.now.getlocal(utc_offset)
+  rescue TZInfo::InvalidTimezoneIdentifier
+    puts 'Invalid timezone'
+  end
+
+  def self.color_text
+    text = File.read(ARGV[1])
+    color_code = COLORS[ARGV[0].to_sym]
+    return puts 'No such color' unless color_code
+
+    puts "\e[#{color_code}m#{text}\e[0m"
+  rescue Errno::ENOENT
+    puts 'No such file'
+  end
 end
+
+HomeWork.timezone
+HomeWork.color_text
