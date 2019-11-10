61d4e0e60fd53e61e61def5998e41c5a77df195f
oksana.skalko <skalkomargo@gmail.com>
2019-11-10 20:29:17 +0300

    add tasks 3, 4, 5

--- lesson_1/home_work_1.rb
+++ lesson_1/home_work_1.rb
@@ -4,25 +4,54 @@ class HomeWork
 
   COLORS = { red: 31, green: 32, yellow: 33, blue: 34, pink: 35 }.freeze
 
-  def self.timezone
-    tzinfo = TZInfo::Timezone.get(ARGV[0])
-    period = tzinfo.current_period
-    utc_offset = period.offset.utc_total_offset
-    puts Time.now.getlocal(utc_offset)
-  rescue TZInfo::InvalidTimezoneIdentifier
-    puts 'Invalid timezone'
-  end
+  class << self
+    def timezone
+      tzinfo = TZInfo::Timezone.get(ARGV[0])
+      period = tzinfo.current_period
+      utc_offset = period.offset.utc_total_offset
+      puts Time.now.getlocal(utc_offset)
+    rescue TZInfo::InvalidTimezoneIdentifier
+      puts 'Invalid timezone'
+    end
 
-  def self.color_text
-    text = File.read(ARGV[1])
-    color_code = COLORS[ARGV[0].to_sym]
-    return puts 'No such color' unless color_code
+    def color_text
+      text = File.read(ARGV[1])
+      color_code = COLORS[ARGV[0].to_sym]
+      return puts 'No such color' unless color_code
 
-    puts "\e[#{color_code}m#{text}\e[0m"
-  rescue Errno::ENOENT
-    puts 'No such file'
+      puts "\e[#{color_code}m#{text}\e[0m"
+    rescue Errno::ENOENT, TypeError
+      puts 'No such file or Invalid color_text'
+    end
+
+    def common_elements
+      puts ARGV[0].split(",") & ARGV[1].split(",")
+    rescue NoMethodError
+      puts 'Invalid array'
+    end
+
+    def memory_size
+      boolean = ARGV[0].eql?('true')
+      integer = boolean ? 1 : 0
+      puts integer.to_s.bytesize
+    end
+
+    def line_number
+      File.open(ARGV[0], "r") do |f|
+        f.each_line.with_index do |line, i|
+          match_line = line.gsub("\n",'').eql?(ARGV[1])
+          puts i + 1 if match_line
+          break if match_line
+        end
+      end
+    rescue Errno::ENOENT
+      puts 'No such file'
+    end
   end
 end
 
 HomeWork.timezone
 HomeWork.color_text
+HomeWork.common_elements
+HomeWork.memory_size
+HomeWork.line_number
